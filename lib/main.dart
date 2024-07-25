cb0518e5cb8449bbec53341db78b384f4942729f
faisal <fhfaisallll@gmail.com>
2024-07-25 19:54:26 +0600

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'dart:html';
+
 import 'package:ecommerce/app/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationReposetory()));
   runApp(
     GetMaterialApp(
       title: "Application",
